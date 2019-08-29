0ac1fae82f6c702f8ff82b7bc62c562073dddd39
alexey1452 <kirickenko1452@gmail.com>
2019-08-29 18:39:36 +0300

    deleted unused controllers, models, migrations, routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -17,22 +17,6 @@ Route::group(['namespace' => 'Api'], function() {
     Route::get('/verify/{code}', 'AuthController@verifyUser');
 
     Route::middleware('auth:api')->group(function (){
-        Route::post('file/upload', 'FileController@uploadFile');
-        Route::delete('file/remove/{fileId}', 'FileController@deleteFile');
-
-        Route::get('/card', 'CardController@index');
-        Route::get('card/{cardId}', 'CardController@show');
-        Route::post('/card', 'CardController@store');
-        Route::put('card/{cardId}', 'CardController@update');
-        Route::delete('card/{cardId}', 'CardController@destroy');
-
-        Route::get('/task', 'TasksController@index');
-        Route::get('task/{taskId}', 'TasksController@show');
-        Route::post('/task', 'TasksController@store');
-        Route::put('task/{taskId}', 'TasksController@update');
-        Route::delete('task/{taskId}', 'TasksController@destroy');
-
-        Route::apiResource('task', 'TasksController');
         Route::apiResource('user', 'UserController');
     });
 });
